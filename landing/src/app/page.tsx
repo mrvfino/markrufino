2eb0d1a3dbf6fd155e4a12be8870e13b74561e38
markr <mark.rufino.io@gmail.com>
2024-03-30 15:11:09 +0800

    wip

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -2,17 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center bg-blue-900">
-      <div className="flex min-h-[80%] w-[80%] flex-row">
-        <div className="w-[61.8%]  p-4">
-          <div className="h-full w-full bg-red-500 p-2">a</div>
-        </div>
-        <div className="flex w-[38.2%] flex-col">
-          <div className="h-[61.8%] p-4">
-            <div className="h-full w-full bg-yellow-500 p-2">b</div>
+    <main className="flex h-screen flex-col items-center justify-center bg-green-400">
+      <div className="flex min-h-[80%] w-[80%] flex-col md:flex-row">
+        <div className="h-[61.8%] p-2 md:h-full md:w-[61.8%] md:p-4">
+          <div className="h-full w-full rounded-2xl border border-black bg-red-500 p-2 shadow-[0px_6px_0px_rgba(0,0,0,1)]">
+            ANG
           </div>
-          <div className="h-[38.2%] p-4">
-            <div className="h-full w-full bg-blue-500 p-2">c</div>
+        </div>
+        <div className="flex h-[38.2%] flex-row-reverse md:h-full md:w-[38.2%] lg:flex-col">
+          <div className="w-[61.8%] p-2 md:p-4 lg:h-[61.8%] lg:w-full">
+            <div className="h-full w-full rounded-2xl border border-black bg-yellow-500 p-2 shadow-[0px_6px_0px_rgba(0,0,0,1)]">
+              BORING
+            </div>
+          </div>
+          <div className="h-full w-[38.2%] p-2 md:h-[38.2%] md:w-full md:p-4">
+            <div className="h-full w-full rounded-2xl border border-black bg-blue-500 p-2 shadow-[0px_6px_0px_rgba(0,0,0,1)]">
+              SOBRA
+            </div>
           </div>
         </div>
       </div>

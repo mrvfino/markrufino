7de35ad462ec11f967dad91f55133b2d49185b4c
markr <mark.rufino.io@gmail.com>
2024-03-30 14:52:38 +0800

    wip

--- landing/src/app/page.tsx
+++ landing/src/app/page.tsx
@@ -3,11 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center bg-blue-900">
-      <div className="flex min-h-[80%] w-[80%] flex-row bg-green-500">
-        <div className="w-[61.8%] bg-red-500">red</div>
+      <div className="flex min-h-[80%] w-[80%] flex-row">
+        <div className="w-[61.8%]  p-4">
+          <div className="h-full w-full bg-red-500 p-2">a</div>
+        </div>
         <div className="flex w-[38.2%] flex-col">
-          <div className="h-[61.8%] bg-yellow-500">yellow</div>
-          <div className="h-[38.2%] bg-blue-500">blue</div>
+          <div className="h-[61.8%] p-4">
+            <div className="h-full w-full bg-yellow-500 p-2">b</div>
+          </div>
+          <div className="h-[38.2%] p-4">
+            <div className="h-full w-full bg-blue-500 p-2">c</div>
+          </div>
         </div>
       </div>
     </main>
